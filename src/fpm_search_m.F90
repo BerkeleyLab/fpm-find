a78098965d304a526076757821dda507d22c004b
Damian Rouson <rouson@lbl.gov>
2026-08-05 20:04:01 -0500

    refact(fpm_search_m): create root module
    Define one module that exports all public entities.
    Move previous src/* files to src/fpm_search. Reference
    only the new root module in the app- and test-directory
    codes.
    
    New src tree:
    
    ├── src
    │   ├── fpm_search
    │   │   ├── indexed_package_m.F90
    │   │   ├── indexed_package_s.F90
    │   │   ├── package_index_m.F90
    │   │   └── package_index_s.F90
    │   └── fpm_search_m.F90

---
+++ src/fpm_search_m.F90
@@ -0,0 +1,9 @@
+! Copyright (c) 2026, The Regents of the University of California
+! Terms of use are as specified in LICENSE.txt
+
+module fpm_search_m
+  !! Fpm_search public entities
+  implicit none
+  use indexed_package_m, only : indexed_package_t
+  use package_index_m, only : package_index_t
+end module fpm_search_m
