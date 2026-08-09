12932549fcfbb4e370303867cd16c62e8989572d
Damian Rouson <rouson@lbl.gov>
2026-08-09 13:59:18 -0700

    chore: global rename search->find; rm fpm-download

--- src/fpm_find_m.F90
+++ src/fpm_find_m.F90
@@ -1,9 +1,9 @@
 ! Copyright (c) 2026, The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 
-module fpm_search_m
-  !! Fpm_search public entities
+module fpm_find_m
+  !! fpm_find public entities
   use indexed_package_m, only : indexed_package_t
   use package_index_m, only : package_index_t
   implicit none
-end module fpm_search_m
+end module fpm_find_m
--- test/driver.F90
+++ test/driver.F90
@@ -3,14 +3,12 @@
 
 program main
   use test_utilities_m   , only : fmt
-  use test_fpm_search_m  , only : test_fpm_search
-  use test_fpm_download_m, only : test_fpm_download
+  use test_fpm_find_m  , only : test_fpm_find
   implicit none
 
   integer :: tests = 0, passes = 0
 
-  call test_fpm_search(tests, passes)
-  call test_fpm_download(tests, passes)
+  call test_fpm_find(tests, passes)
 
   print fmt(tests), new_line(''), passes, " of ", tests, " tests passed in total. "// "____" // new_line('') 
 
